117d3af421367221f88fb247a4b68bb0f2eb226b
juyonglee <juyonglee0208@gmail.com>
2023-02-09 19:29:47 +0900

    App Commponent 구조 수정

--- controlled-uncontrolled-example/src/App.js
+++ controlled-uncontrolled-example/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import MyControlledComponent from "./components/MyControlledComponent";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <MyControlledComponent/>
       </header>
     </div>
   );
