f41e0779e3dcf7975083ff98378fa8d6646d9217
juyonglee <juyonglee0208@gmail.com>
2023-02-09 19:27:58 +0900

    Controlled Component에 Button 추가

--- controlled-uncontrolled-example/src/components/MyControlledComponent.jsx
+++ controlled-uncontrolled-example/src/components/MyControlledComponent.jsx
@@ -10,6 +10,7 @@ class MyControlledComponent extends React.Component {
     return (
         <div>
           <input type="text" value={inputValue} onChange={this.changeHandler}/>
+          <button onClick={this.clickHandler}>전송</button>
         </div>
     )
   }
@@ -18,6 +19,10 @@ class MyControlledComponent extends React.Component {
     this.setState({
       input_value: e.target.value
     })
+  }
+
+  clickHandler = () => {
+    alert(`전송된 값: ${this.state.input_value}`)
   }
 }
 
