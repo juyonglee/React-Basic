3221391a69d5107edff1e52637d4722a3cdc01eb
juyonglee <juyonglee0208@gmail.com>
2023-02-10 09:04:39 +0900

    UnControlled Component 추가

--- controlled-uncontrolled-example/src/App.js
+++ controlled-uncontrolled-example/src/App.js
@@ -1,15 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import MyControlledComponent from "./components/MyControlledComponent";
+import MyUncontrolledComponent from "./components/MyUncontrolledComponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <MyControlledComponent/>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <MyControlledComponent/>
+          <MyUncontrolledComponent/>
+        </header>
+      </div>
   );
 }
 
