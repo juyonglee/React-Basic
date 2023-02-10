c3565fd346cbe8e57644c237a60b260fe58e816d
juyonglee <juyonglee0208@gmail.com>
2023-02-10 09:32:48 +0900

    Reference를 활용하도록 Component 변경

--- controlled-uncontrolled-example/src/components/MyUncontrolledComponent.jsx
+++ controlled-uncontrolled-example/src/components/MyUncontrolledComponent.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 
 class MyUncontrolledComponent extends React.Component {
+
+  myInputRef = React.createRef()
+
   render() {
+    console.log("[render]", this.myInputRef)
     return (
         <div>
-          <input id="my-input"/>
+          <input ref={this.myInputRef}/>
           <button onClick={this.clickHandler}>전송</button>
         </div>
     )
   }
 
+  componentDidMount() {
+    console.log("[componentDidMount]", this.myInputRef)
+  }
+
   clickHandler = () => {
-    const inputEL = document.getElementById("my-input")
-    alert(`전송된 값: ${inputEL.value}`)
+    alert(`전송된 값: ${this.myInputRef.current.value}`)
   }
 }
 
