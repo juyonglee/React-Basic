f3d179651d1943c8b43fc2b994464cc860516fe7
juyonglee <juyonglee0208@gmail.com>
2023-02-11 23:50:46 +0900

    Life Cycle DependencyList 추가

---
+++ Hooks/basic-hook-example/src/components/lifecycle/MyFuncLifeEx.jsx
@@ -0,0 +1,70 @@
+import React, {useEffect, useState} from "react";
+
+function MyFuncLifeEx() {
+
+  const [state, setState] = useState({
+    count: 0
+  })
+
+  useEffect(() => {
+    console.log("[useEffect] componentDidMount", state.count)
+    console.log("state:", state.count)
+  }, [])
+  import React, {useEffect, useState} from "react";
+
+  function MyFuncLifeEx() {
+
+    const [state, setState] = useState({
+      count: 0
+    })
+
+    useEffect(() => {
+      console.log("[useEffect] componentDidMount", state.count)
+      console.log("state:", state.count)
+    }, [])
+
+    useEffect(() => {
+      console.log("[useEffect] componentDidMount & componentDidUpdate by count")
+      console.log("state:", state.count)
+    }, [state.count])
+
+    const clickHandler = (e) => {
+      setState(prevState => {
+        return {
+          count: prevState.count + 1
+        }
+      })
+    }
+
+    return (
+        <div>
+          <p>You Clicked {state.count} times!</p>
+          <button onClick={clickHandler}>Click Me</button>
+        </div>
+    )
+  }
+
+  export default MyFuncLifeEx;
+
+  useEffect(() => {
+    console.log("[useEffect] componentDidMount & componentDidUpdate by count")
+    console.log("state:", state.count)
+  }, [state.count])
+
+  const clickHandler = (e) => {
+    setState(prevState => {
+      return {
+        count: prevState.count + 1
+      }
+    })
+  }
+
+  return (
+      <div>
+        <p>You Clicked {state.count} times!</p>
+        <button onClick={clickHandler}>Click Me</button>
+      </div>
+  )
+}
+
+export default MyFuncLifeEx;
