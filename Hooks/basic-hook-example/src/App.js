4b04ae159594822850f499d84480f280a91b69de
juyonglee <juyonglee0208@gmail.com>
2023-02-11 23:51:53 +0900

    App Commponent 구조 수정

--- Hooks/basic-hook-example/src/App.js
+++ Hooks/basic-hook-example/src/App.js
@@ -3,17 +3,23 @@ import './App.css';
 import MyClassCompExample from "./components/state/MyClassCompExample";
 import MyFunctionCompExample from "./components/state/MyFunctionCompExample";
 import MyHookFunctionComponent from "./components/state/MyHookFunctionComponent";
+import MyClassCompLifeEx from "./components/lifecycle/MyClassCompLifeEx";
+import MyFuncLifeEx from "./components/lifecycle/MyFuncLifeEx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <MyClassCompExample/>
-        <MyFunctionCompExample/>
-        <MyHookFunctionComponent/>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h3>State Example</h3>
+          <MyClassCompExample/>
+          <MyFunctionCompExample/>
+          <MyHookFunctionComponent/>
+          <h3>Life Cycle Example</h3>
+          <MyClassCompLifeEx/>
+          <MyFuncLifeEx/>
+        </header>
+      </div>
   );
 }
 
