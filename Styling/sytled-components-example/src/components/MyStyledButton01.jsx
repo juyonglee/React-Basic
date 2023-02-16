7bb27e8516ecd312dcd65a8f7b1f26691745859c
juyonglee <juyonglee0208@gmail.com>
2023-02-17 08:04:46 +0900

    Style을 적용한 Button Component 생성

---
+++ Styling/sytled-components-example/src/components/MyStyledButton01.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const MyStyledButton01 = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: solid 2px palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`
+
+export default MyStyledButton01
