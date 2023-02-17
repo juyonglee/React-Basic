39a6f97aa81a5b9846f4f8394defd102ba1c741a
juyonglee <juyonglee0208@gmail.com>
2023-02-17 09:30:59 +0900

    기존의 Component를 이용하여 Style을 적용한 Button

---
+++ Styling/sytled-components-example/src/components/MyPrimaryStyledButton.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import MyStyledButton02 from "./MyStyledButton02";
+
+const MyPrimaryStyledButton = styled(MyStyledButton02)`
+  background-color: palevioletred;
+  color: white;
+`
+
+export default MyPrimaryStyledButton;
