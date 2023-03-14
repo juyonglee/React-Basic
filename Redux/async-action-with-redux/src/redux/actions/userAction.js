ca7d59e69469bf759928de33f0404cc70353202e
juyonglee <juyonglee0208@gmail.com>
2023-03-14 09:15:34 +0900

    Action Type 정의

---
+++ Redux/async-action-with-redux/src/redux/actions/userAction.js
@@ -0,0 +1,6 @@
+// Github API Call 시작을 의미합니다.
+const GET_USERS_START = "GET_USERS_START"
+// Github API Call 성공을 의미합니다.
+const GET_USERS_SUCCESS = "GET_USERS_SUCCESS"
+// Github API Call 실패를 의미합니다.
+const GET_USERS_FAIL = "GET_USERS_FAIL"
