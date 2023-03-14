7eda436e2ee56499bb44e77befa60dbd3be5b100
juyonglee <juyonglee0208@gmail.com>
2023-03-14 11:38:51 +0900

    Asynchronous Login 변경

--- Redux/async-action-with-redux/src/containers/UserListContainer.jsx
+++ Redux/async-action-with-redux/src/containers/UserListContainer.jsx
@@ -1,7 +1,26 @@
 import UserList from "../components/UserList";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {useCallback} from "react";
+import {
+  getUsersFailActionCreator,
+  getUsersStartActionCreator,
+  getUsersSuccessActionCreator
+} from "../redux/actions/userAction";
 
 export default function UserListContainer() {
-  const userInfos = useSelector(state => state.user.data)
-  return <UserList userInfos={userInfos}/>
+  const userInfos = useSelector(state => {
+    return state.user.userInfos
+  })
+
+  const dispatch = useDispatch()
+
+  const getUserInfos = useCallback(async () => {
+    dispatch(getUsersStartActionCreator())
+    fetch("https://api.github.com/users")
+    .then(response => response.json())
+    .then(userInfo => dispatch(getUsersSuccessActionCreator(userInfo)))
+    .catch(errorInfo => dispatch(getUsersFailActionCreator(errorInfo)))
+  }, [dispatch])
+
+  return (<UserList userInfos={userInfos} getUserInfos={getUserInfos}/>)
 }
