c74d9f8db51bce8b30076c84fa09f88f36b79697
juyonglee <juyonglee0208@gmail.com>
2023-03-14 09:25:07 +0900

    Action Export

--- Redux/async-action-with-redux/src/redux/actions/userAction.js
+++ Redux/async-action-with-redux/src/redux/actions/userAction.js
@@ -1,9 +1,9 @@
 // Github API Call 시작을 의미합니다.
-const GET_USERS_START = "GET_USERS_START"
+export const GET_USERS_START = "GET_USERS_START"
 // Github API Call 성공을 의미합니다.
-const GET_USERS_SUCCESS = "GET_USERS_SUCCESS"
+export const GET_USERS_SUCCESS = "GET_USERS_SUCCESS"
 // Github API Call 실패를 의미합니다.
-const GET_USERS_FAIL = "GET_USERS_FAIL"
+export const GET_USERS_FAIL = "GET_USERS_FAIL"
 
 export function getUsersStartActionCreator() {
   return {
