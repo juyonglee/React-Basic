a37f0698955e3e6c7d6e365705160904e6d7b07b
juyonglee <juyonglee0208@gmail.com>
2023-03-14 11:18:15 +0900

    User List Component 추가

--- Redux/async-action-with-redux/src/components/UserList.jsx
+++ Redux/async-action-with-redux/src/components/UserList.jsx
@@ -1,4 +1,14 @@
-export default function UserList({userInfos}) {
+import {useEffect} from "react";
+
+export default function UserList({userInfos, start, fail, success}) {
+  useEffect(() => {
+    start()
+    fetch("https://api.github.com/users")
+    .then(response => response.json())
+    .then(userInfo => success(userInfo))
+    .catch(error => fail(error))
+  }, [start, success, fail])
+  
   if (userInfos.length === 0) {
     return <h5>Github User 정보가 존재하지 않습니다.</h5>
   }
