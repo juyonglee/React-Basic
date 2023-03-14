1d5114acf174c25a6ef609ac38837aac04ec43ff
juyonglee <juyonglee0208@gmail.com>
2023-03-14 09:23:45 +0900

    userReducer Action Creator 추가

--- Redux/async-action-with-redux/src/redux/actions/userAction.js
+++ Redux/async-action-with-redux/src/redux/actions/userAction.js
@@ -4,3 +4,23 @@ const GET_USERS_START = "GET_USERS_START"
 const GET_USERS_SUCCESS = "GET_USERS_SUCCESS"
 // Github API Call 실패를 의미합니다.
 const GET_USERS_FAIL = "GET_USERS_FAIL"
+
+export function getUsersStartActionCreator() {
+  return {
+    type: GET_USERS_START
+  }
+}
+
+export function getUsersSuccessActionCreator(userInfos) {
+  return {
+    type: GET_USERS_SUCCESS,
+    userInfos: userInfos
+  }
+}
+
+export function getUsersFailActionCreator(errorInfo) {
+  return {
+    type: GET_USERS_FAIL,
+    errorInfo: errorInfo
+  }
+}
