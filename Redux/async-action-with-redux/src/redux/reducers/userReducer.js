3dbca8f7f61d236238b328231895466c9505f3d9
juyonglee <juyonglee0208@gmail.com>
2023-03-14 09:30:57 +0900

    userReducer 구현

--- Redux/async-action-with-redux/src/redux/reducers/userReducer.js
+++ Redux/async-action-with-redux/src/redux/reducers/userReducer.js
@@ -1,3 +1,9 @@
+import {
+  GET_USERS_FAIL,
+  GET_USERS_START,
+  GET_USERS_SUCCESS
+} from "../actions/userAction";
+
 const initialState = {
   loading: false,
   data: [],
@@ -5,5 +11,25 @@ const initialState = {
 };
 
 export default function userReducer(state = initialState, action) {
+  if (action.type === GET_USERS_START) {
+    return {
+      ...state,
+      loading: true,
+      error: null
+    }
+  } else if (action.type === GET_USERS_SUCCESS) {
+    return {
+      ...state,
+      data: action.userInfos,
+      loading: false
+    }
+  } else if (action.type === GET_USERS_FAIL) {
+    return {
+      ...state,
+      data: [],
+      loading: false,
+      error: action.errorInfo
+    }
+  }
   return state
 }
