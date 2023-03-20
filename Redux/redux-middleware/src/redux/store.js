4dcf58f56bf02eb14d3cac747a0cff4af60e6447
juyonglee <juyonglee0208@gmail.com>
2023-03-21 08:53:23 +0900

    Redux-Store에 Middleware 설정

--- Redux/redux-middleware/src/redux/store.js
+++ Redux/redux-middleware/src/redux/store.js
@@ -1,4 +1,17 @@
-import {createStore} from "redux";
+import {applyMiddleware, createStore} from "redux";
 import {rootReducer} from "./reducer/rootReducer";
 
-export const store = createStore(rootReducer)
+function middleware01(store) {
+  console.log("Middleware01", 0)
+  return (next) => {
+    console.log("Middleware01", 1)
+    return (action) => {
+      console.log("Middleware01", 2)
+      const returnValue = next(action)
+      console.log("Middleware01", 3)
+      return returnValue
+    }
+  }
+}
+
+export const store = createStore(rootReducer, applyMiddleware(middleware01))
