18d857b9d3d9dcfb7dd4425c529565a93c322be6
juyonglee <juyonglee0208@gmail.com>
2023-03-21 10:16:34 +0900

    import 문제 수정

--- Redux/redux-thunk/src/redux/reducers/rootReducer.js
+++ Redux/redux-thunk/src/redux/reducers/rootReducer.js
@@ -1,5 +1,4 @@
 import {combineReducers} from "redux";
 import userReducer from "./userReducer";
-import userReducer from "./userReducer";
 
 export const rootReducer = combineReducers({github: userReducer})
