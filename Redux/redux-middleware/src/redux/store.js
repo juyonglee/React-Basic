9324d729f8d4d115fe74b67826fb0a016070ea14
juyonglee <juyonglee0208@gmail.com>
2023-03-21 09:19:48 +0900

    Middleware 추가

--- Redux/redux-middleware/src/redux/store.js
+++ Redux/redux-middleware/src/redux/store.js
@@ -8,10 +8,24 @@ function middleware01(store) {
     return (action) => {
       console.log("Middleware01", 2)
       const returnValue = next(action)
+      console.log(returnValue)
       console.log("Middleware01", 3)
       return returnValue
     }
   }
 }
 
-export const store = createStore(rootReducer, applyMiddleware(middleware01))
+function middleware02(store) {
+  console.log("Middleware02", 0)
+  return (next) => {
+    console.log("Middleware02", 1)
+    return (action) => {
+      console.log("Middleware02", 2)
+      const returnValue = next(action)
+      console.log("Middleware02", 3)
+      return returnValue
+    }
+  }
+}
+
+export const store = createStore(rootReducer, applyMiddleware(middleware01, middleware02))
